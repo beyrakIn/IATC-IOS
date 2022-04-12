c6beb3eb03d7febda3e4e0b2be77171f61b9f674
Beyrak <pc1@172-10-123-10.lightspeed.nworla.sbcglobal.net>
2022-04-12 04:37:38 -0700

    All task are ready.

--- Session1/ViewController.swift
+++ Session1/ViewController.swift
@@ -11,9 +11,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        printFriendName("Beyrak")
+        printNameAndAddress("Beyrak", "Baku")
+        
+        let randNumber = getRandomNumber(10, 20)
+        if randNumber % 4 == 0{
+          print(randNumber)
+        }
+
+        let randNumber2 = getRandomNumber(0, 100)
+        
+        if randNumber2 < 22 {
+            print("\(randNumber2) smaller than 22.")
+        }else{
+            print("\(randNumber2) bigger than 22.")
+        }
+        
+        print("Reminder is \(randNumber2 % 2)")
+        let x = getRandomNumber(0, 100)
+        let y = getRandomNumber(0, 100)
+        let z = getRandomNumber(0, 100)
+        print("Sum of three random number is \(sum(x, y, z))")
+        
+
     }
 
+    func printFriendName(_ name: String) {
+        print("My friend's name is \(name)")
+    }
+    
+    func printNameAndAddress(_ name: String,_ address: String) {
+        print("Name is \(name)")
+        print("Address is \(address)")
+    }
+
+    func getRandomNumber(_ min: Int, _ max: Int) -> Int {
+        return Int.random(in: min..<max)
+    }
+    
+    func sum(_ x:Int, _ y:Int,_ z:Int) -> Int{
+        x + y + z
+    }
 
 }
 
