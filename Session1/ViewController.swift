e72253c4cfe9dca5d9bae1a0c1447a7d50d9fba2
Beyrak <pc1@172-10-123-10.lightspeed.nworla.sbcglobal.net>
2022-04-16 04:27:06 -0700

    UIView, UIButton, UILabel

--- Session1/ViewController.swift
+++ Session1/ViewController.swift
@@ -9,12 +9,44 @@ import UIKit
 import Swift
 
 class ViewController: UIViewController {
+    
+    private lazy var label: UILabel = {
+        var label = UILabel()
+        label.text = "Helllo world!"
+        label.textColor = .orange
+        label.frame = CGRect.init(x: 40, y: 50, width: 100, height: 20)
+        return label
+    }()
+    
+    private lazy var button: UIButton = {
+        var button = UIButton()
+        button.setTitle("Random.", for: .normal)
+        button.backgroundColor = .orange
+        button.frame = CGRect.init(x: 40, y: 80, width: 100, height: 35)
+        button.layer.cornerRadius = 10
+        
+        button.addTarget(self, action: #selector(buttonOnClick), for: .allEvents)
+        return button
+    }()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-                
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.addSubview(label)
+        self.view.addSubview(button)
         
+        
+    }
+    
+    @objc func buttonOnClick(){
+        let randNum = Int.random(in: 0...100)
+        label.text = String(randNum)
     }
     
 
